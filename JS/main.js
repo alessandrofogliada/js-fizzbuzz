e813403b11e845c4b86d1ffe76a0da426cf1b82d
alessandrofogliada <fogliada.a@gmail.com>
2024-06-09 12:10:16 +0200

    completamento dell esercizio

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,34 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-const container = document.querySelector(".container")
+// Associo la section di HTML a js
 
-// crea una ripetizione di numeri con for che vada dallo 0 al 100
-for (let i = 1; i <= 100; i++){
-    console.log(i)
-// creare contenitore dei numeri
-    const element = `<div class="box">${i}</div>`;
-    console.log(element);
-// indichiamo che l'elemento va inserito nel container
-    container.innerHTML +=element;
-
-    if ( element === 3 ) {
-        box.classList.add("fizz")
-    } 
-}
-
+let container = document.getElementById('container');
 
 // crea una variabile con if , else per modificare i numeri multipli di 3 e 5
 
-const element = document.getElementsByClassName("i")
+for (let i = 1; i <= 100; i++) {
+// creo un elemento in modo da poter creare uuna serie di div
+    let cartellaColorata = document.createElement("div");
+// assegno per ogni caso i valori richiesti
+if (i % 3 === 0 && i % 5 === 0){
+   cartellaColorata.style.backgroundColor = "green";
+   cartellaColorata.textContent = "Fizz";
 
+} else if (i % 3 === 0) {
+    cartellaColorata.style.backgroundColor = "yellow";
+    cartellaColorata.textContent = "Buzz";
+
+} else if (i % 5 === 0) {
+    cartellaColorata.style.backgroundColor = "red";
+    cartellaColorata.textContent = "FizzBuzz";
+
+} else { 
+    cartellaColorata.style.backgroundColor = "blue";
+    cartellaColorata.textContent = i;
+}
+// appendo le informazione create ai div
+     container.append(cartellaColorata);
+}
 
 
 
